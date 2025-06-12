5d84a0551649a15197e73ee4a2f93cda065fed6d
Asim kamal <asimkamalk@gmail.com>
2025-06-12 05:19:59 +0500

    added horizontal role and heading

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
 </head>
 
 <body class="bg-blue-100">
+    <h3 class="text-2xl ml-10 bg-green-100  rounded-2xl p-5 w-32 ">Account Opening</h3>
     <div>
         <a href="{{ route('account.create')  }}">
             <button class="mt-10 ml-10 bg-green-500 px-1 py-2 rounded-2xl cursor-pointer">
@@ -21,6 +22,7 @@
             <button class="mt-10 ml-10 bg-green-500 px-4 py-2 rounded-2xl cursor-pointer">Account List</button>
         </a>
     </div>
+    <hr class="mt-5">
 </body>
 
 </html>
